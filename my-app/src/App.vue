4b89395b6d1908bc9303b3aca7ff32ef37a9fda3
tung17122k <sanking1712@gmail.com>
2024-07-04 16:49:56 +0700

    init project

---
+++ my-app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ComHeader></ComHeader>
+    <div v-show="isShow">
+      <h1>Lap trinh</h1>
+    </div>
+    <button @click="isShow = !isShow">An/hien</button>
+  </div>
+</template>
+
+<script>
+import ComHeader from "./components/ComHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    ComHeader,
+  },
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-app/src/components/ComHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>day la menu</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
