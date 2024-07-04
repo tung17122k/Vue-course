3551c8baccf13d77b74ea6847e4b7af9c6fd6c3b
tung17122k <sanking1712@gmail.com>
2024-07-04 17:42:14 +0700

    add youtubeList

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <ComHeader></ComHeader>
+    <ComHeader
+      image="https://images.unsplash.com/photo-1719176372344-b29f6613e870?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      title="Leaning Vue"
+    ></ComHeader>
     <div v-show="isShow">
       <h1>Lap trinh</h1>
     </div>
@@ -24,13 +27,4 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- my-app/src/components/ComHeader.vue
+++ my-app/src/components/ComHeader.vue
@@ -1,14 +1,125 @@
 <template>
   <div>
-    <h2>day la menu</h2>
+    <div class="youtube-list">
+      <div class="youtube-item">
+        <div class="youtube-image">
+          <img :src="image" alt="" />
+        </div>
+        <div class="youtube-footer">
+          <img
+            src="https://images.unsplash.com/photo-1719209618812-a11aea8d1a32?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            class="youtube-avatar"
+          />
+          <div class="youtube-info">
+            <h3 class="youtube-title">{{ title }}</h3>
+            <h4 class="youtube-author">Tung</h4>
+          </div>
+        </div>
+      </div>
+      <div class="youtube-item">
+        <div class="youtube-image">
+          <img :src="image" alt="" />
+        </div>
+        <div class="youtube-footer">
+          <img
+            src="https://images.unsplash.com/photo-1719209618812-a11aea8d1a32?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            class="youtube-avatar"
+          />
+          <div class="youtube-info">
+            <h3 class="youtube-title">{{ title }}</h3>
+            <h4 class="youtube-author">Tung</h4>
+          </div>
+        </div>
+      </div>
+      <div class="youtube-item">
+        <div class="youtube-image">
+          <img :src="image" alt="" />
+        </div>
+        <div class="youtube-footer">
+          <img
+            src="https://images.unsplash.com/photo-1719209618812-a11aea8d1a32?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            class="youtube-avatar"
+          />
+          <div class="youtube-info">
+            <h3 class="youtube-title">{{ title }}</h3>
+            <h4 class="youtube-author">Tung</h4>
+          </div>
+        </div>
+      </div>
+      <div class="youtube-item">
+        <div class="youtube-image">
+          <img :src="image" alt="" />
+        </div>
+        <div class="youtube-footer">
+          <img
+            src="https://images.unsplash.com/photo-1719209618812-a11aea8d1a32?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            class="youtube-avatar"
+          />
+          <div class="youtube-info">
+            <h3 class="youtube-title">{{ title }}</h3>
+            <h4 class="youtube-author">Tung</h4>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: {},
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.youtube-list {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  padding: 25px;
+}
+.youtube-image {
+  height: 250px;
+  margin-bottom: 15px;
+}
+.youtube-image img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.youtube-footer {
+  display: flex;
+  align-items: flex-start;
+  gap: 0 20px;
+}
+.youtube-avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  object-fit: cover;
+  flex-shrink: 0;
+}
+.youtube-title {
+  font-size: 18px;
+  margin-bottom: 5px;
+}
+.youtube-author {
+  font-size: 14px;
+  color: #999;
+  font-weight: 300;
+}
+</style>
