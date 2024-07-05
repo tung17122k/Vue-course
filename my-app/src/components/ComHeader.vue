2bcb79b53722bfd9501fcf2cb01e67213206c713
VT1712 <tungnv171220@gmail.com>
2024-07-05 09:00:37 +0700

    update youtubeList

--- my-app/src/components/ComHeader.vue
+++ my-app/src/components/ComHeader.vue
@@ -1,70 +1,20 @@
 <template>
   <div>
-    <div class="youtube-list">
       <div class="youtube-item">
         <div class="youtube-image">
           <img :src="image" alt="" />
         </div>
         <div class="youtube-footer">
           <img
-            src="https://images.unsplash.com/photo-1719209618812-a11aea8d1a32?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            :src="avatar"
             alt=""
             class="youtube-avatar"
           />
           <div class="youtube-info">
             <h3 class="youtube-title">{{ title }}</h3>
-            <h4 class="youtube-author">Tung</h4>
+            <h4 class="youtube-author">{{ author }}</h4>
           </div>
         </div>
-      </div>
-      <div class="youtube-item">
-        <div class="youtube-image">
-          <img :src="image" alt="" />
-        </div>
-        <div class="youtube-footer">
-          <img
-            src="https://images.unsplash.com/photo-1719209618812-a11aea8d1a32?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-            class="youtube-avatar"
-          />
-          <div class="youtube-info">
-            <h3 class="youtube-title">{{ title }}</h3>
-            <h4 class="youtube-author">Tung</h4>
-          </div>
-        </div>
-      </div>
-      <div class="youtube-item">
-        <div class="youtube-image">
-          <img :src="image" alt="" />
-        </div>
-        <div class="youtube-footer">
-          <img
-            src="https://images.unsplash.com/photo-1719209618812-a11aea8d1a32?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-            class="youtube-avatar"
-          />
-          <div class="youtube-info">
-            <h3 class="youtube-title">{{ title }}</h3>
-            <h4 class="youtube-author">Tung</h4>
-          </div>
-        </div>
-      </div>
-      <div class="youtube-item">
-        <div class="youtube-image">
-          <img :src="image" alt="" />
-        </div>
-        <div class="youtube-footer">
-          <img
-            src="https://images.unsplash.com/photo-1719209618812-a11aea8d1a32?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-            class="youtube-avatar"
-          />
-          <div class="youtube-info">
-            <h3 class="youtube-title">{{ title }}</h3>
-            <h4 class="youtube-author">Tung</h4>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -80,18 +30,21 @@ export default {
       type: String,
       required: true,
     },
+    author: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.youtube-list {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 25px;
-  padding: 25px;
-}
+
 .youtube-image {
   height: 250px;
   margin-bottom: 15px;
